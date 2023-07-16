34ed27894b608ab7336b7c5b88ae6b743c39f59f
Kittof <ocsifej2002@gmail.com>
2023-07-16 22:24:49 +0200

    porting ipynb to py

--- cah.py
+++ cah.py
@@ -4,26 +4,30 @@ import re
 translator = Translator(to_lang="hu")
 regex = r"\bMYMEMORY\b"
 
-with open("translations/eng/CAH Base Set Full.json","r",encoding="utf8") as f:
+with open("translations/eng/CAH Base Sett Partitial.json","r",encoding="utf8") as f:
     eng = json.load(f)
 
-hun = {"white" : [],"black" : []}
 restofeng = {"white" : [],"black" : []}
+
+with open("translations/hu/EEK Alapszett részleges.json","r",encoding="utf8") as f:
+    hun = json.load(f)
 
 i = 1
 
 for card in eng["white"]:
-    translation = translator.translate(card["text"])
+    translation = translator.translate(card)
+    print(card)
     if re.search(regex,translation):
-        restofeng["white"].append(card["text"])
-        print(str(i)+". : "+card["text"])
+        restofeng["white"].append(card)
+        print(str(i)+". : "+card)
         i+=1
         with open("translations/eng/CAH Base Sett Partitial.json","w") as f:
             f.write(json.dumps(restofeng))
     else:
-        kartya = {"hun" : translation,"eng" : card["text"]}
+        kartya = {"hun" : translation,"eng" : card}
         hun["white"].append(kartya)
-        print(str(i)+". : "+kartya["hun"]+" / "+kartya["eng"])
+        print(str(i)+". : "+kartya)
         i+=1
         with open("translations/hu/EEK Alapszett részleges.json","w") as f:
             f.write(json.dumps(hun))
+    
