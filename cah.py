970ea0ff153b22d38938cbdf76edef6f9836a3b2
DnyKristof <danyi.kristof1998@gmail.com>
2023-07-17 13:59:18 +0200

    white cards done

--- cah.py
+++ cah.py
@@ -4,7 +4,7 @@ import re
 translator = Translator(to_lang="hu")
 regex = r"\bMYMEMORY\b"
 
-with open("translations/eng/CAH Base Sett Partitial.json","r",encoding="utf8") as f:
+with open("translations/eng/CAH Base Set Full.json","r",encoding="utf8") as f:
     eng = json.load(f)
 
 restofeng = {"white" : [],"black" : []}
@@ -14,20 +14,20 @@ with open("translations/hu/EEK Alapszett részleges.json","r",encoding="utf8") a
 
 i = 1
 
-for card in eng["white"]:
+for card in eng["black"]:
+    card=card["text"]
     translation = translator.translate(card)
     if re.search(regex,translation):
-        restofeng["white"].append(card)
+        restofeng["black"].append(card)
         print(str(i)+". : "+card)
         i+=1
         with open("translations/eng/CAH Base Sett Partitial.json","w") as f:
             f.write(json.dumps(restofeng))
     else:
         kartya = {"hun" : translation,"eng" : card}
-        hun["white"].append(kartya)
+        hun["black"].append(kartya)
         print(str(i)+". : "+kartya["hun"] +" / "+kartya["eng"])
         i+=1
         with open("translations/hu/EEK Alapszett részleges.json","w") as f:
-            f.write(json.dumps(hun))
-    
+            f.write(json.dumps(hun))    
     
